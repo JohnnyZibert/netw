5cb2920fbdd545785cebf5da558c686c2a8998ed
kurva <zgirdan@mail.ru>
2022-01-11 23:35:14 +0300

    123123

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import { Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
-    return ( <div>
+    return (<div>
             <div className='app-wrapper'>
-                <Header />
-                <Navbar />
+                <Header/>
+                <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
-                           render={ () => <DialogsContainer store={props.store} /> }/>
+                           render={() => <DialogsContainer store={props.store}/>}/>
 
                     <Route path='/profile'
-                           render={ () => <Profile store={props.store} /> }/>
+                           render={() => <Profile store={props.store}/>}/>
                 </div>
             </div>
-    </div>
-        )
+        </div>
+    )
 }
 
 export default App;
